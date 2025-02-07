ef4447cefb5e12de42146899a5038508922d76f8
Julio Jaimes <carlosjuliojr@hotmail.com>
2025-02-06 23:08:07 -0300

    hello world

--- src/main/java/helloworld/HelloWorld.java
+++ src/main/java/helloworld/HelloWorld.java
@@ -1,4 +1,18 @@
 package helloworld;
 
+/**
+ * The type Hello world.
+ */
 public class HelloWorld {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+
 }
