f0e93b48f150d83a3e43630b38e729043eda78e5
Julio Jaimes <carlosjuliojr@hotmail.com>
2025-02-08 11:20:02 -0300

    boolean variable

---
+++ src/main/java/variables/BooleanPrimitives.java
@@ -0,0 +1,26 @@
+package variables;
+
+/**
+ * The type Boolean primitives.
+ */
+public class BooleanPrimitives {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+
+        boolean flag = false;
+        System.out.println("********\tVariable type Boolean\t*******************");
+        System.out.println("flag = " + flag);
+
+
+        if(!false){
+            System.out.println("flag is " + flag);
+        }
+
+
+    }
+}
